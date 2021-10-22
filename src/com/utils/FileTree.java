9e5b383f71a6be3f974ad49ef6575ad1dda203f4
Francis-xsc <62462293+Francis-xsc@users.noreply.github.com>
2021-10-22 22:30:14 +0800

    将字符串转为MD5码

---
+++ src/com/utils/FileTree.java
@@ -0,0 +1,48 @@
+package com.utils;
+
+import java.io.File;
+import java.util.*;
+import java.util.List;
+
+/**
+ * @author xsc
+ * @time 2021/10/22 - 16:21
+ */
+public class FileTree {
+
+    /**
+     * 生成文件目录树
+     * windows自带 cmd-> tree /f
+     * @param file  文件夹
+     * @param depth
+     */
+    public static void ftree(File file, int depth) {
+        List<File> files = Arrays.asList(file.listFiles());
+        files.sort(new Comparator<File>() {
+            @Override
+            public int compare(File o1, File o2) {
+                if (o1.isDirectory() && o2.isFile()) {
+                    return -1;
+                }
+                if (o1.isFile() && o2.isDirectory()) {
+                    return 1;
+                }
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        for (int i = 0; i < files.size(); i++) {
+            for (int j = 0; j < depth; j++) {
+                System.out.print("|\t");
+            }
+            if (i == files.size() - 1) {
+                System.out.print("└──\t");
+                System.out.println(files.get(i).getName());
+            } else {
+                System.out.println("├──\t" + files.get(i).getName());
+            }
+            if (files.get(i).isDirectory()) {
+                ftree(files.get(i), depth + 1);
+            }
+        }
+    }
+}
---
+++ src/com/utils/MD5Util.java
@@ -0,0 +1,45 @@
+package com.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author xsc
+ * @time 2020/12/17 - 11:02
+ */
+
+/**
+ * MD5工具类
+ */
+public class MD5Util
+{
+    private static final String slat = "&#5123***&&%%$$#@";
+
+    /**
+     * 将字符串转为MD5码，用于保存密码
+     * @param dataStr 要转换的字符串
+     *
+     * @return MD5码
+     */
+    public static String tomd5(String dataStr)
+    {
+        try
+        {
+            dataStr = dataStr + slat;
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.update(dataStr.getBytes(StandardCharsets.UTF_8));
+            byte s[] = m.digest();
+            String result = "";
+            for (int i = 0; i < s.length; i++)
+            {
+                result+=Integer.toHexString((0x000000FF & s[i]) | 0xFFFFFF00).substring(6);
+            }
+            return result;
+        } catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
